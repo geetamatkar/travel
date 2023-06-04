a95ff85d482fe4225c373036454e883f6fd3d6c7
geetamatkar <geetamatkar1@gmail.com>
2023-06-04 16:28:22 -0500

    #Added menu options

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,8 @@ const [click, setClick] = useState(false)
 
 const handleClick = () => setClick(!click)
 
+const closeMobileMenu = () => setClick(false)
+
   return (
     <>
       <nav className='navbar'>
@@ -17,6 +19,32 @@ const handleClick = () => setClick(!click)
             <div className='menuIcon' onClick={handleClick}>
               <i className={click ? 'fa-solid fa-xmark' : 'fa-sharp fa-solid fa-bars' } />
             </div>
+
+            <ul className={click? 'nav-menu active' : 'nav-menu'}>
+              <li className='nav__item'>
+                <Link to='/' className='nav__links' onClick={closeMobileMenu}>
+                  Home
+                </Link>
+              </li>
+
+              <li className='nav__item'>
+                <Link to='/services' className='nav__links' onClick={closeMobileMenu}>
+                  Services
+                </Link>
+              </li>
+
+              <li className='nav__item'>
+                <Link to='/products' className='nav__links' onClick={closeMobileMenu}>
+                  Products
+                </Link>
+              </li>
+
+              <li className='nav__item'>
+                <Link to='/sign-up' className='nav__links-mobile' onClick={closeMobileMenu}>
+                  Sign up
+                </Link>
+              </li>
+            </ul>
             
 
         </div>
